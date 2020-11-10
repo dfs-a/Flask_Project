ba66c911dc2e07a0855719f9af387f28d86cdced
dongfengsu <62462089+dfs-a@users.noreply.github.com>
2020-11-10 17:11:47 +0800

    three_commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+#导入config配置文件
+from config import Config
 #将Flask类的实例，赋值给名为app的变量，这个实例成为app包的成员
 app = Flask(__name__)
 #__name__打印的是那个包或者模块在使用我
-
+app.config.from_object(Config)
+print(app.config['SECRET_KEY'])
+#从app包中导入routes
 from app import routes
