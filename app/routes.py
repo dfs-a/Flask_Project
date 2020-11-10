2ce00823a16ea28351b7d92b3abef990d822b73e
dongfengsu <62462089+dfs-a@users.noreply.github.com>
2020-11-10 15:59:47 +0800

    two_commit

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,20 @@
 from app import app
+from flask import render_template
 #两个路由
 @app.route('/')
 @app.route('/index')
 #一个视图函数
 def index():
-    return "初始页"
+    user = {'username':'dfs'}
+    # 创建一个列表：帖子。里面元素是两个字典，每个字典里元素还是字典，分别作者、帖子内容。
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+    {
+        'author': {'username': 'Susan'},
+        'body': 'The Avengers movie was so cool!'
+    }
+    ]
+    return render_template('index.html',title='home',name=user,posts=posts)
